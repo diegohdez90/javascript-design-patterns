abd6c8c0590858ca21269db63d10cea64646813a
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-06-17 11:55:59 -0600

    feat(3.+The+Singletones+pattern+with+a+simpler+syntax): add project

---
+++ 3.+The+Singletones+pattern+with+a+simpler+syntax/codes-after/src/Product.js
@@ -0,0 +1,11 @@
+var logger = require("./Logger");
+
+class Product {
+  constructor(name, inventory = []) {
+    this.name = name;
+    this.inventory = inventory;
+    logger.log(`New Product: ${name} has ${inventory.length} items in stock.`);
+  }
+}
+
+module.exports = Product;
