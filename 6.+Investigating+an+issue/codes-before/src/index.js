1e5bb084c2ede6c044da6375a910d19721d2ed91
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-06-17 13:28:36 -0600

    feat(6.+Investigating+an+issue): add project

---
+++ 6.+Investigating+an+issue/codes-before/src/index.js
@@ -0,0 +1,10 @@
+var Customer = require("./Customer");
+
+// Employees
+var john = new Customer("John", true, true, 50);
+var bill = new Customer("Bill", true, false, 30);
+var max = new Customer("Max", true, false);
+
+// Customers
+var frank = new Customer("Frank", false, false, 0, 600, ["shorts", "shoes", s]);
+var ali = new Customer("Ali", false, false, 0, 1100);
