23e792ed1a2dfce71324127d5a68bc52289eafbc
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-07-29 13:33:11 -0600

    feat(7.+Fixing+the+issue+using+the+Builder+pattern): add builder class

--- 7.+Fixing+the+issue+using+the+Builder+pattern/codes-before/src/index.js
+++ 7.+Fixing+the+issue+using+the+Builder+pattern/codes-before/src/index.js
@@ -1,14 +1,32 @@
-var PersonBuilder = require("./PersonBuilder");
-
+var PersonBuilder = require('./PersonBuilder')
 // Employees
-var john = new PersonBuilder("John").makeEmployee().makeManager(50).build();
-var bill = new PersonBuilder("Bill").makeEmployee().makeManager(30).build();
-var max = new PersonBuilder("Max").makeEmployee().build();
+var john = new PersonBuilder("John")
+                .makeEmployee()
+                .makeManager(50)
+                .build();
+
+var bill = new PersonBuilder("Bill")
+                .makeEmployee()
+                .makeManager(30)
+                .build();
+
+var max = new PersonBuilder("Max")
+                .makeEmployee()
+                .build();
 
 // Customers
 var frank = new PersonBuilder("Frank")
-  .withFunds(600)
-  .withList(["shorts", "shoes"])
-  .build();
+                .withFunds(600)
+                .withList([
+                  'shorts',
+                  'shoes'
+                ])
+                .build();
 
-var ali = new PersonBuilder("Ali").withFunds(400).withList([]).build();
+var ali = new PersonBuilder("Ali")
+                .withFunds(400)
+                .withList([])
+                .build();
+
+console.log(john.toString())
+console.log(frank.toString())
