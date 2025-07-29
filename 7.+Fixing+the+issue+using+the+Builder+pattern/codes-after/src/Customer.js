4a24035d8261ceedf56e3a65a174bde514516740
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-07-29 12:59:12 -0600

    feat(7.+Fixing+the+issue+using+the+Builder+pattern): init project

---
+++ 7.+Fixing+the+issue+using+the+Builder+pattern/codes-after/src/Customer.js
@@ -0,0 +1,16 @@
+class Customer {
+  constructor(builder) {
+    this.name = builder.name;
+    this.isEmployee = builder.isEmployee;
+    this.isManager = builder.isManager;
+    this.hours = builder.hours || 0;
+    this.funds = builder.funds || 0;
+    this.shoppingList = builder.shoppingList || [];
+  }
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
+
+module.exports = Customer;
---
+++ 7.+Fixing+the+issue+using+the+Builder+pattern/codes-after/src/index.js
@@ -0,0 +1,17 @@
+var PersonBuilder = require("./PersonBuilder");
+
+// Employees
+var john = new PersonBuilder("John").makeEmployee().makeManager(50).build();
+var bill = new PersonBuilder("Bill").makeEmployee().makeManager(30).build();
+var max = new PersonBuilder("Max").makeEmployee().build();
+
+// Customers
+var frank = new PersonBuilder("Frank")
+  .withFunds(600)
+  .withList(["shorts", "shoes"])
+  .build();
+
+var ali = new PersonBuilder("Ali").withFunds(400).withList([]).build();
+
+console.log(john.toString());
+console.log(frank.toString());
